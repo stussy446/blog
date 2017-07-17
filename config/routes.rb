8a0e554d4662e059f76df9de04966b7e8a91950c
Steve Rector <steverector27@gmail.com>
2017-07-16 23:19:06 -0700

    Switch order of resources and root in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-
   resources :comments
   resources :posts
+
+  root 'posts#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
